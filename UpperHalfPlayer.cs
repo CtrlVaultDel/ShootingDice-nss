dc4cba30620167f0ff6815aa2b2303e3b69296ca
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-03-15 14:28:16 -0500

    UpperHalfPlayer added

--- UpperHalfPlayer.cs
+++ UpperHalfPlayer.cs
@@ -1,10 +1,14 @@
+using System;
+
 namespace ShootingDice
 {
-    // TODO: Complete this class
-
     // A Player whose role will always be in the upper half of their possible rolls
-    public class UpperHalfPlayer
+    public class UpperHalfPlayer : Player
     {
-
+        public override int Roll()
+        {
+            // Return a random number that falls in the upper half range of the original dice size
+            return new Random().Next((DiceSize / 2) + 1, DiceSize + 1);
+        }
     }
 }
